8d563d925912f9bf8eaa6a4d767f87760904d513
cgdgalvin <cg.dgalvin@gmail.com>
2022-08-16 16:02:15 +0100

    Update BrowserSpecification.java

--- src/test/java/generic/BrowserSpecification.java
+++ src/test/java/generic/BrowserSpecification.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserSpecification {
-
-    public static WebDriver driver;
+    
+    private void setChomeDriverProperty() {
+     System.setProperty("webdriver.chrome.driver",  System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver.exe");
+    }
 
     public static WebDriver getDriver() {
-        String projectPath = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver", projectPath+"/src/test/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        return driver;
+        setChomeDriverProperty()
+        return new ChromeDriver();;
     }
 }
